e02970bf9ad71171dbe8f907f42188490d9bc12f
ijassiem <ijassiem@ska.ac.za>
2019-11-01 15:16:10 +0200

    added print formats

---
+++ print_formats.py
@@ -0,0 +1,17 @@
+num1 = 10
+num2 = 3.34
+word = 'Complete'
+
+print(num1)
+
+print(num1,num2,word)
+
+print(str(num1) + word)
+
+print('Quanity = ', num1)
+
+print('Quantity = %d and Value = %f' %(num1, num2))
+
+print("Test status: %s" %word)
+
+
